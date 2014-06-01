e0bee1f85cadba0c649fb4e8fff221f9f5d56d71
jannickep <jannicke.p@hotmail.com>
2014-06-01 15:14:04 -0700

    Naming update to LaneDector

---
+++ sb_lanes/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes/test_results"
+export CMAKE_PREFIX_PATH="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes:$ROS_PACKAGE_PATH"
+export ROS_TEST_RESULTS_DIR="/home/jannicke/snowbots_ws/src/IGVC2014/sb_lanes/test_results"
