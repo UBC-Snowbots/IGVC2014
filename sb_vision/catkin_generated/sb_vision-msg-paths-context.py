d9c4fb983e2f8acd48a71b9e89617625796d7212
Navid <navid.fattahi@alumni.ubc.ca>
2014-06-01 13:53:09 -0700

    removed snowbots_ws it is machine-specific. put the packages in your own catkin workspace and use

---
+++ sb_vision/catkin_generated/sb_vision-msg-paths-context.py
@@ -0,0 +1,8 @@
+# generated from genmsg/cmake/pkg-msg-paths.context.in
+
+DEVELSPACE = 'TRUE' == 'TRUE'
+INSTALLSPACE = 'FALSE' == 'TRUE'
+
+PROJECT_NAME = 'sb_vision'
+PKG_MSG_INCLUDE_DIRS = '/home/jannicke/catkin_ws/src/sb_vision/msg'
+ARG_DEPENDENCIES = 'std_msgs'
---
+++ sb_vision/cmake/sb_vision-genmsg-context.py
@@ -0,0 +1,10 @@
+# generated from genmsg/cmake/pkg-genmsg.context.in
+
+messages_str = "/home/jannicke/catkin_ws/src/sb_vision/msg/Num.msg"
+services_str = ""
+pkg_name = "sb_vision"
+dependencies_str = "std_msgs"
+langs = "gencpp;genlisp;genpy"
+dep_include_paths_str = "sb_vision;/home/jannicke/catkin_ws/src/sb_vision/msg;std_msgs;/opt/ros/hydro/share/std_msgs/cmake/../msg"
+PYTHON_EXECUTABLE = "/usr/bin/python"
+package_has_static_sources = '' == 'TRUE'
